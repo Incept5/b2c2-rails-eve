c13c98e4dbb1d224ecf68b5d716a6078fff3cb13
TiGz <ajchesney@gmail.com>
2024-12-19 15:37:14 +0000

    Update header title to use translation.

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -13,7 +13,7 @@ export function Header(props: IProps) {
   return (
     <div className="fixed left-0 top-0 flex w-full items-center justify-between border bg-slate-50 bg-opacity-70 px-4 py-4 md:px-12">
       <a href="/" className="text-xs md:text-base">
-        Vite React TS Tailwind Starter
+        {t('title')}
       </a>
       <div className="flex items-center gap-4">
         <LanguageSelector />
